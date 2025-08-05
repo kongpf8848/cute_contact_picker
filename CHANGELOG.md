cfcabfd291bc96940dabf6fbbb10a48425f0578a
kongpf8848 <kongpf8848@gmail.com>
2025-08-05 09:54:58 +0800

    update version to 0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,5 @@
 # CHANGELOG
 
-## 0.1.0(2025-08-03)
-
+## 0.1.0(2025-08-05)
 * Initial first release.
-* support Android、IOS、HarmonyOS Platform
+
--- test/cute_contact_picker_test.dart
+++ test/cute_contact_picker_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:cute_contact_picker/cute_contact_picker.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('plugins.flutter.io/cute_contact_picker');
